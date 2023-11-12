7bf411faf0f429f44d108c860616b70b17796a5f
Matthew Kasper <Mkasper045@gmail.com>
2023-11-11 20:40:12 -0600

    change ports and addresses

--- BackEnd/src/main/java/com/example/backend/utility/Constants.java
+++ BackEnd/src/main/java/com/example/backend/utility/Constants.java
@@ -2,5 +2,5 @@ package com.example.backend.utility;
 
 public class Constants {
     public static final String databasePath = "C:\\Users\\Mkasp\\repos\\MadHacks\\BackEnd\\src\\main\\resources\\TestDatabase.json";
-    public static final String origin = "http://127.0.0.1:5500";
+    public static final String origin = "http://10.128.0.2:80";
 }
