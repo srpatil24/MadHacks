5d016ed70ef588c71d87c3335f6f525340c04391
Matthew Kasper <Mkasper045@gmail.com>
2023-11-11 21:14:52 -0600

    change ip

--- BackEnd/src/main/java/com/example/backend/utility/Constants.java
+++ BackEnd/src/main/java/com/example/backend/utility/Constants.java
@@ -2,5 +2,5 @@ package com.example.backend.utility;
 
 public class Constants {
     public static final String databasePath = "C:\\Users\\Mkasp\\repos\\MadHacks\\BackEnd\\src\\main\\resources\\TestDatabase.json";
-    public static final String origin = "http://35.184.184.255:80";
+    public static final String origin = "http://10.128.0.2:80";
 }
