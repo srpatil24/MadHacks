4ff780d1cf8779605f7d21c0bfcbca14bd37e73e
Matthew Kasper <Mkasper045@gmail.com>
2023-11-11 23:08:37 -0600

    Update Constants.java

--- BackEnd/src/main/java/com/example/backend/utility/Constants.java
+++ BackEnd/src/main/java/com/example/backend/utility/Constants.java
@@ -1,6 +1,6 @@
 package com.example.backend.utility;
 
 public class Constants {
-    public static final String databasePath = "~/madhacks/MadHacks/BackEnd/src/main/resources/TestDatabase.json";
+    public static final String databasePath = "home/madhacks698/madhacks/MadHacks/BackEnd/src/main/resources/TestDatabase.json";
     public static final String origin = "*";
 }
