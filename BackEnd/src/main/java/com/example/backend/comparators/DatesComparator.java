020abe70a0a1264228e17e54c33885e30fb0f373
Matthew Kasper <Mkasper045@gmail.com>
2023-11-12 02:43:49 -0600

    Update DatesComparator.java

--- BackEnd/src/main/java/com/example/backend/comparators/DatesComparator.java
+++ BackEnd/src/main/java/com/example/backend/comparators/DatesComparator.java
@@ -9,8 +9,8 @@ public class DatesComparator implements Comparator<Post> {
 
     @Override
     public int compare(Post o1, Post o2) {
-        String[] o1DateSplice = o1.date.split("-");
-        String[] o2DateSplice = o2.date.split("-");
+        String[] o1DateSplice = o1.date.substring(0, 10).split("-");
+        String[] o2DateSplice = o2.date.substring(0, 10).split("-");
 
         Date date1 = new Date(Integer.parseInt(o1DateSplice[0]), Integer.parseInt(o1DateSplice[1]) - 1, Integer.parseInt(o1DateSplice[2]));
         Date date2 = new Date(Integer.parseInt(o2DateSplice[0]), Integer.parseInt(o2DateSplice[1]) - 1, Integer.parseInt(o2DateSplice[2]));
