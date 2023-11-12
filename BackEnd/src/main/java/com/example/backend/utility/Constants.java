a1feb9c080877357858860cc5b811d678367faff
Matthew Kasper <Mkasper045@gmail.com>
2023-11-11 22:50:16 -0600

    Add wildcard to origin

--- BackEnd/src/main/java/com/example/backend/utility/Constants.java
+++ BackEnd/src/main/java/com/example/backend/utility/Constants.java
@@ -2,5 +2,5 @@ package com.example.backend.utility;
 
 public class Constants {
     public static final String databasePath = "/home/madhacks698/madhacks/MadHacks/BackEnd/src/main/resourcesTestDatabase.json";
-    public static final String origin = "";
+    public static final String origin = "*";
 }
