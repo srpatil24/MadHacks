d40cbbfeab3ef502678924a62a79c7530cb5167c
Matthew Kasper <Mkasper045@gmail.com>
2023-11-12 01:50:40 -0600

    Update PostService.java

--- BackEnd/src/main/java/com/example/backend/posts/PostService.java
+++ BackEnd/src/main/java/com/example/backend/posts/PostService.java
@@ -14,47 +14,6 @@ import java.security.InvalidParameterException;
 import java.util.Collections;
 import java.util.List;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-/**
- *
- * @author sqlitetutorial.net
- */
-public class Connect {
-     /**
-     * Connect to a sample database
-     */
-    public static void connect() {
-        Connection conn = null;
-        try {
-            // db parameters
-            String url = "jdbc:sqlite:C:/sqlite/db/chinook.db";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-            
-            System.out.println("Connection to SQLite has been established.");
-            
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        connect();
-    }
-}
 @Component
 /**
  * Maintians posts database and offers services for grabbing posts and getting the database.
